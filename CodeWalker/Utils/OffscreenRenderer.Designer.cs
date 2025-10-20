28ffc05d1d0509e26cf64c34d6071a2d244d31af
DarkSoe <darksoe@cheekyfox.de>
2025-10-20 06:59:39 +0200

    start offscreenrenderer

---
+++ CodeWalker/Utils/OffscreenRenderer.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace CodeWalker.Utils
+{
+    partial class OffscreenRenderer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OffscreenRenderer));
+            this.SuspendLayout();
+            // 
+            // OffscreenRenderer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 444);
+            this.ControlBox = false;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "OffscreenRenderer";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "OffscreenRenderer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
--- CodeWalker/Utils/OffscreenRenderer.cs
+++ CodeWalker/Utils/OffscreenRenderer.cs
@@ -1,60 +1,20 @@
-﻿using CodeWalker.Rendering;
-using CodeWalker.World;
-using SharpDX.Direct3D11;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CodeWalker.Utils
 {
-    public class OffscreenRenderer : DXForm
+    public partial class OffscreenRenderer : Form
     {
-        public Renderer Renderer = null;
-
         public OffscreenRenderer()
         {
-        }
-
-        public Form Form => throw new NotImplementedException();
-
-        public void BuffersResized(int w, int h)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void CleanupScene()
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool ConfirmQuit()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void InitScene(Device device)
-        {
-            int width = 350;
-            int height = 350;
-
-            try
-            {
-                Renderer.DeviceCreated(device, width, height);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error loading shaders!\n" + ex.ToString());
-                return;
-            }
-        }
-
-        public void RenderScene(DeviceContext context)
-        {
-            throw new NotImplementedException();
+            InitializeComponent();
         }
     }
 }
